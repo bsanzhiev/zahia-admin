a9a105bdbb61c08f44bb705919bb5dbecee88786
baatar <bsanzhiev@outlook.com>
2023-09-25 10:48:07 +0800

    Hello test gpg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -37,6 +37,9 @@ export default function Home() {
           height={37}
           priority
         />
+      </div>
+      <div>
+        <h1>Hello Dashboard</h1>
       </div>
 
       <div className={styles.grid}>
@@ -91,5 +94,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
